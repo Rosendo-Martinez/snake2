e058e726a29f0d1eb8ee8fe7335afc0ef424f51f
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-11-18 20:36:15 -0600

    Refactor SnakeGUI::drawGrid()

--- src/SnakeGUI.cpp
+++ src/SnakeGUI.cpp
@@ -41,39 +41,34 @@ void SnakeGUI::clear()
  */
 void SnakeGUI::drawGrid()
 {
-    sf::Color color(255, 255, 255, 50);
-    const int gridHeightInCells = windowHeight/cellHeight - 1;
-    sf::VertexArray horizontalLines(sf::Lines, gridHeightInCells * 2);
+    sf::Color color(255, 255, 255, 30);
+    
+    const int hLinesCount = windowHeight/cellHeight - 1;
+    sf::VertexArray hLines(sf::Lines, hLinesCount * 2);
 
-    for (int i = 0; i < gridHeightInCells; i++)
+    for (int i = 0; i < hLinesCount; i++)
     {
-        int firstVertexIndex = i * 2;
-        int secondVertexIndex = i * 2 + 1;
+        hLines[i * 2].position = sf::Vector2f(0, cellHeight * (i + 1));
+        hLines[i * 2 + 1].position = sf::Vector2f(windowWidth, cellHeight * (i + 1));
 
-        horizontalLines[firstVertexIndex].position = sf::Vector2f(0, cellHeight * (i + 1));
-        horizontalLines[secondVertexIndex].position = sf::Vector2f(windowWidth, cellHeight * (i + 1));
-
-        horizontalLines[firstVertexIndex].color = color;
-        horizontalLines[secondVertexIndex].color = color;
+        hLines[i * 2].color = color;
+        hLines[i * 2 + 1].color = color;
     }
 
-    const int gridWidthInCells = windowWidth/cellWidth - 1;
-    sf::VertexArray verticalLines(sf::Lines, gridWidthInCells * 2);
+    const int vLinesCount = windowWidth/cellWidth - 1;
+    sf::VertexArray vLines(sf::Lines, vLinesCount * 2);
 
-    for (int i = 0; i < gridWidthInCells; i++)
+    for (int i = 0; i < vLinesCount; i++)
     {
-        int firstVertexIndex = i * 2;
-        int secondVertexIndex = i * 2 + 1;
+        vLines[i * 2].position = sf::Vector2f(cellWidth * (i + 1), 0);
+        vLines[i * 2 + 1].position = sf::Vector2f(cellWidth * (i + 1), windowHeight);
 
-        verticalLines[firstVertexIndex].position = sf::Vector2f(cellWidth * (i + 1), 0);
-        verticalLines[secondVertexIndex].position = sf::Vector2f(cellWidth * (i + 1), windowHeight);
-
-        verticalLines[firstVertexIndex].color = color;
-        verticalLines[secondVertexIndex].color = color;
+        vLines[i * 2].color = color;
+        vLines[i * 2 + 1].color = color;
     }
 
-    m_window.draw(horizontalLines);
-    m_window.draw(verticalLines);
+    m_window.draw(hLines);
+    m_window.draw(vLines);
 }
 
 /**
